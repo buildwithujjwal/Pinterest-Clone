1156cbe19f47ccda1790a63d4db3f7d4449aa90f
buildwithujjwal <ujjwal.work04@gmail.com>
2026-03-30 03:12:02 +0530

    fix seed file: reduce posts per user to match Unsplash image limit

--- seed.js
+++ seed.js
@@ -12,7 +12,7 @@ const seedDb = async function(){
     await Post.deleteMany({});
 
     const response = await fetch(
-      "https://api.unsplash.com/photos/random?count=50&client_id=" +
+      "https://api.unsplash.com/photos/random?count=30&client_id=" +
         process.env.UNSPLASH_ACCESS_KEY,
     );
 
@@ -21,6 +21,7 @@ const seedDb = async function(){
     const imageUrls = data.map(function(photo){
       return photo.urls.regular;
     });
+    console.log("Total images fetched:", imageUrls.length);
 
     for (let i = 0; i < 10; i++) {
       const user = await User.register(
@@ -34,7 +35,7 @@ const seedDb = async function(){
 
       await user.save();
 
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i < 3; i++) {
         const post = new Post({
           user: user._id,
           image: imageUrls[Math.floor(Math.random() * imageUrls.length)],
